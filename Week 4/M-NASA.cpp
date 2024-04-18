07e88a406cdba4223cd94fbc33d58e3fee8ac359
Sumaiya <sumaiyatasnim5548@gmail.com>
2024-04-18 11:09:31 -0700

    Update M-NASA.cpp

--- Week 4/M-NASA.cpp
+++ Week 4/M-NASA.cpp
@@ -8,7 +8,8 @@ using namespace std;
 const ll INF = 1e18;
 const ll MOD = 1e9+7;
 
-const int N = 2e5+5;
+const int N = (1<<15);
+vector<int> allPal;
 
 
 
@@ -33,10 +34,47 @@ int countSqrt(int l, int r){
 //*------------------------*//
 
 
+void findPalindrome()
+{
+    for(int i=0 ; i<N ; i++){
+        string s = to_string(i);
+        int n = s.size();
+
+        bool ok = true;
+        int l=0, r=n-1;
+        while(l<r){
+            if(s[l]!=s[r]){
+                ok = false;
+                break;
+            }
+            l++;
+            r--;
+        }
+        if(ok){
+            allPal.push_back(i);
+        }
+    }
+}
+
 
 void Solve()
 {
-    
+    int n;
+    cin >> n;
+    vector<int> a(n), cnt(N);
+    for(int i=0 ; i<n ; i++){
+        cin >> a[i];
+        cnt[a[i]]++;
+    }
+
+    ll ans = n;
+    for(int i=0 ; i<n ; i++){
+        for(int j=0 ; j<allPal.size() ; j++){
+            int curr = (a[i]^allPal[j]);
+            ans += cnt[curr];
+        }
+    }
+    cout << ans/2 << "\n";
 }
 
 int main()
@@ -44,6 +82,7 @@ int main()
     Faster;
     int t;
     cin >> t;
+    findPalindrome();
     while(t--){
         Solve();
     }
