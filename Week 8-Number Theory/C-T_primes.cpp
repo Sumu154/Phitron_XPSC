129cefb4537d0c7a0c0094335e0247b8cc7ed2e7
Sumaiya <sumaiyatasnim5548@gmail.com>
2024-06-12 00:01:45 +0600

    2nd commit

---
+++ Week 8-Number Theory/C-T_primes.cpp
@@ -0,0 +1,98 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define Faster ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+const ll INF = 1e18;
+const ll MOD = 1e9+7;
+
+const int N = 2e5+5;
+
+
+
+//*-------------------------*//
+int countSqrt(int l, int r){
+    ll m = ceil(sqrt(l));
+    ll n = floor(sqrt(r));
+    return n-m+1;
+}
+
+int BinarytoDec(string s){
+    int k = s.size();
+    int ans = 0;
+    for(int i=0 ; i<k ; i++){
+        ans |= (s[i]-'0') << (k-1-i);
+    }
+    return ans;
+}
+
+string decToBinary(int n){
+    string ans;
+    while(n){
+        if(n&1){
+            ans += '1';
+        }
+        else{
+            ans += '0';
+        }
+        n >>= 1;
+    }
+    reverse(ans.begin(), ans.end());
+    return ans;
+}
+
+//*------------------------*//
+
+
+
+bool isPrime(ll n)
+{
+    if(n==1){
+        return false;
+    }
+    for(int ll i=2 ; i*i<=n ; i++){
+        if(n%i == 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+bool isPerfectSquare(ll n)
+{
+    ll x = sqrtl(n);
+    return x*x == n;
+}
+
+
+
+void Solve()
+{
+    int n;
+    cin >> n;
+    vector<ll> a(n);
+    for(int i=0 ; i<n ; i++){
+        cin >> a[i];
+    }
+
+    for(int i=0 ; i<n ; i++){
+        if(isPrime(sqrtl(a[i])) and isPerfectSquare(a[i])){
+            cout << "YES\n";
+        }
+        else{
+            cout << "NO\n";
+        }
+    }
+
+    
+}
+int main()
+{
+    Faster;
+    
+    Solve();
+    
+}
+
---
+++ Week 8-Number Theory/H-Taxes.cpp
@@ -0,0 +1,92 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define Faster ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+const ll INF = 1e18;
+const ll MOD = 1e9+7;
+
+const int N = 2e5+5;
+
+
+
+//*-------------------------*//
+int countSqrt(int l, int r){
+    ll m = ceil(sqrt(l));
+    ll n = floor(sqrt(r));
+    return n-m+1;
+}
+
+int BinarytoDec(string s){
+    int k = s.size();
+    int ans = 0;
+    for(int i=0 ; i<k ; i++){
+        ans |= (s[i]-'0') << (k-1-i);
+    }
+    return ans;
+}
+
+string decToBinary(int n){
+    string ans;
+    while(n){
+        if(n&1){
+            ans += '1';
+        }
+        else{
+            ans += '0';
+        }
+        n >>= 1;
+    }
+    reverse(ans.begin(), ans.end());
+    return ans;
+}
+
+//*------------------------*//
+
+
+
+bool isPrime(ll n)
+{
+    if(n==1){
+        return false;
+    }
+    for(int ll i=2 ; i*i<=n ; i++){
+        if(n%i == 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+void Solve()
+{
+    ll n;
+    cin >> n;
+    if(isPrime(n)){
+        cout << 1 << "\n";
+    }
+    else if(n%2 == 0){
+        cout << 2 << "\n";
+    }
+    else{
+        if(isPrime(n-2)){
+            cout << 2 << "\n";
+        }
+        else{
+            cout << 3 << "\n";
+        }
+    }
+
+    
+    
+}
+int main()
+{
+    Faster;
+    
+    Solve();
+    
+}
+
