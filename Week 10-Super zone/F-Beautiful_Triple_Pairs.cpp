643e3e93a96cf74a669aad74a57090becfd0f6ab
Sumu154 <sumaiyatasnim5548@gmail.com>
2024-07-12 22:43:57 +0600

    commit

---
+++ Week 10-Super zone/F-Beautiful_Triple_Pairs.cpp
@@ -0,0 +1,87 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+template<typename T> using pbds = tree<T,null_type,less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+
+#define ll long long
+#define ld long double
+#define Faster ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+const ll INF = 1e18;
+const ll MOD = 1e9+7;
+
+const int N = 2e5+5;
+
+
+
+//*-------------------------*//
+int countSqrt(int l, int r){
+    ll m = ceil(sqrt(l));
+    ll n = floor(sqrt(r));
+    return n-m+1;
+}
+
+int BinarytoDec(string s){
+    int k = s.size();
+    int ans = 0;
+    for(int i=0 ; i<k ; i++){
+        ans |= (s[i]-'0') << (k-1-i);
+    }
+    return ans;
+}
+
+string decToBinary(int n){
+    string ans;
+    while(n){
+        if(n&1){
+            ans += '1';
+        }
+        else{
+            ans += '0';
+        }
+        n >>= 1;
+    }
+    reverse(ans.begin(), ans.end());
+    return ans;
+}
+
+//*------------------------*//
+
+
+
+void Solve()
+{
+    int n;
+    cin >> n;
+    vector<ll> a(n);
+    for(int i=0 ; i<n ; i++){
+        cin >> a[i];
+    }
+
+    map<tuple<ll,ll,ll>,ll> mp;
+    ll ans = 0;
+    for(int i=0 ; i<n-2 ; i++){
+        ans += mp[{a[i],a[i+1],-1}]+mp[{a[i],-1,a[i+2]}]+mp[{-1,a[i+1],a[i+2]}] - 3*mp[{a[i],a[i+1],a[i+2]}];
+        mp[{a[i],a[i+1],-1}]++;
+        mp[{a[i],-1,a[i+2]}]++;
+        mp[{-1,a[i+1],a[i+2]}]++;
+        mp[{a[i],a[i+1],a[i+2]}]++;
+    }
+
+    cout << ans << "\n";
+}
+
+int main()
+{
+    Faster;
+    int t;
+    cin >> t;
+    while(t--){
+        Solve();
+    }
+}
+
